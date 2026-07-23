70744ee68d033e4a608116172637218b733e9831
Mustafa Ahmed <mustafa.softcode@gmail.com>
2026-07-23 05:57:31 +0300

    Add Firebase config for GitHub Pages

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAU7vLujPOewhsYPCX4Zl41n_PZ4425iSo",
+  authDomain: "tower-management-4417c.firebaseapp.com",
+  projectId: "tower-management-4417c",
+  messagingSenderId: "500984480372",
+  appId: "1:500984480372:web:5f2418db8e4525d57ac721",
+  usernameEmailDomain: "",
+};
